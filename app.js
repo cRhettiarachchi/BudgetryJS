197eeac45812eb9aa34fffcdef2cf097b54be788
Charith.r <charith.rhettiarachchi@gmail.com>
2019-04-27 17:06:41 +0530

    partial completion of updating the bottom of the ui

--- app.js
+++ app.js
@@ -53,18 +53,48 @@ var UIcontroller = (function(){
         type: '.add__type',
         desc: '.add__description',
         value: '.add__value',
-        add: '.add__btn'
+        add: '.add__btn',
+        incomeContainer: 'income__list', // used as rep in the code to replace if it is an income 
+        expenseContainer: 'expenses__list' // used as rep in the code to replace if it is an expense
+
     }
 
     return { 
         readValues : function(){ // reading user input
-            values = {
+            values = { // the object that reads all the values 
                 type: document.querySelector(selectors.type).value,
                 desc: document.querySelector(selectors.desc).value,
                 value: document.querySelector(selectors.value).value,
             }
-            return(values); // return of the radValue function
+            return(values); // return the value object to the controller module 
         },
+
+        updateUI: function(obj, type){ // this is a public function which can be accessed by the control module to update the ui of the page 
+            var html, newHtml, rep;
+
+            // 1. create html tags
+
+            if(type === 'inc'){
+                // 
+                rep = selectors.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%desc%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            else if(type === 'exp'){
+                rep = selectors.expenseContainer;
+                html= '<div class="item clearfix" id="expense-%id%"><div class="item__description">%desc%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            // 2. allocate value dynamically
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%desc%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // 3. add html to the dom
+
+            document.querySelector('.' + rep).insertAdjacentHTML('beforeend', newHtml); // using a insertAdjacentHTML function 
+
+        }, // end of updateUI function 
 
         domSelectors: function(){ // to return the selectors object 
             return(selectors); // return of the selector object
@@ -87,6 +117,7 @@ var controller = (function(appCtrl, UIctrl){
         if(event.key === "Enter"){
             triggered();
         }
+
     })
 
     }
@@ -94,12 +125,12 @@ var controller = (function(appCtrl, UIctrl){
     function triggered(){
         // get the value to variables 
         var userInput = UIctrl.readValues();
-        console.log(userInput);
 
-        // update the appController with teh data
+        // update the appController with the data and gets the returning object
         var value = appCtrl.storeValue(userInput.type, userInput.desc, userInput.value);
 
         // update the bottom ui of the page 
+        UIctrl.updateUI(value, userInput.type);
 
         // add or reduce the amount from the the monthly income 
 
